ccf97b3826be50d15b37682f3db71582fb21dfec
Ellen Lo <lowing@bu.edu>
2017-05-17 19:03:15 +0800

    Seconds function

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,20 +8,30 @@ addButton.addEventListener("click", function(){
 		var li = document.createElement("li");
 		li.setAttribute("class", "task");
 		var p = document.createElement("p");
+		var start = 0;
+		var seconds = setInterval(function(){
+			start += 1;
+			
+		}, 1000);
 		p.innerHTML = "<div class = \"task-name\">" + taskName + 
-							"</div><div class = \"task-right\"><a href=\"#\"><img id = \"task-pause\" src=\"./images/Group 5.png\"></a><a href=\"#\"><img id = \"task-done\" src=\"./images/Group 4.png\"></a></div>";
-		/*
-		var divTaskName = document.createElement("div");
-		divTaskName.setAttribute("class", "task-name");
-		divTaskName.innerHTML = taskName;
-		var divTaskRight = document.createElement("div");
-		divTaskRight.setAttribute("class", "task-right");
-		divTaskRight.innerHTML = "<a href="/"><img id = "task-pause" src="./images/Group 5.png"></a><a href="/"><img id = "task-done" src="./images/Group 4.png"></a>";
+							"</div><div class = \"task-right\"><a href=\"#\"><img id = \"task-pause\" src=\"./images/Group 5.png\"></a><a href=\"#\"><img id = \"task-done\" src=\"./images/Group 4.png\"></a></div> " + start + " seconds";
 		
-		p.appendChild(divTaskName);
-		p.appendChild(divTaskRight);
-		*/
+
 		li.appendChild(p);
 		tasks.appendChild(li);
 });
-		
+
+
+
+
+var clicked = false;
+var pauseButton = document.getElementById("task-pause");
+
+/*pauseButton.addEventListener("click", function(){
+	if(!clicked){
+		var x = window.setTimeout()
+		clicked = true;
+	}
+});
+
+*/
