28e60095df0f6b470fa95051d157d012910f8836
Ellen Lo <lowing@bu.edu>
2017-05-17 18:25:47 +0800

    Add button functionality enabled

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,27 @@
-function addTask(){
-	var addButton = document.getElementByClassName("addButton");
-	var task = document.getElementByClassName('task');
-	var tasks = document.getElementByClassName('tasks');
-	addButton.onclick = function(){
-		tasks.appendChild(task);
-	}
-}
+var addButton = document.getElementById("addButton");
+var text = document.getElementById('text');
+addButton.addEventListener("click", function(){
+
+		var tasks = document.getElementById('tasks');
+		var taskName = text.value;
+		text.value = "";
+		var li = document.createElement("li");
+		li.setAttribute("class", "task");
+		var p = document.createElement("p");
+		p.innerHTML = "<div class = \"task-name\">" + taskName + 
+							"</div><div class = \"task-right\"><a href=\"#\"><img id = \"task-pause\" src=\"./images/Group 5.png\"></a><a href=\"#\"><img id = \"task-done\" src=\"./images/Group 4.png\"></a></div>";
+		/*
+		var divTaskName = document.createElement("div");
+		divTaskName.setAttribute("class", "task-name");
+		divTaskName.innerHTML = taskName;
+		var divTaskRight = document.createElement("div");
+		divTaskRight.setAttribute("class", "task-right");
+		divTaskRight.innerHTML = "<a href="/"><img id = "task-pause" src="./images/Group 5.png"></a><a href="/"><img id = "task-done" src="./images/Group 4.png"></a>";
+		
+		p.appendChild(divTaskName);
+		p.appendChild(divTaskRight);
+		*/
+		li.appendChild(p);
+		tasks.appendChild(li);
+});
+		
